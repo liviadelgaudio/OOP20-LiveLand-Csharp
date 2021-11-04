9f76cc42ed0f16cc72e57525afe652eef83d2c16
liviadelgaudio <livia.delgaudio@studio.unibo.it>
2021-11-04 10:36:57 +0100

    csharp project created

---
+++ CSharp-Livia/CSharp-Livia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Livia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
